150f9116ab236a25831cbfc5916c9f16fccaadc0
Marcus F <marcusf.dev@gmail.com>
2025-05-05 11:02:25 +0100

    Fix: Issues regarding cloning.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import discord
 import os
+import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
