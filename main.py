994aec92833df6c7cb90865078b837a3041a906e
Marcus F <marcusf.dev@gmail.com>
2025-05-05 11:34:10 +0100

    Add: Bot message detection & token handling.

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 
 client = Client(intents=intents)
-client.run('DISCORD_TOKEN')
+
+token = os.getenv("DISCORD_TOKEN")
+
+if not token:
+    raise ValueError("⚠️ DISCORD_TOKEN is not set in the enviroment")
+else:
+    client.run(token)
