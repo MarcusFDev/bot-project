a0d579b97259b0e22754d6201798c3dcd32095b2
Marcus F <marcusf.dev@gmail.com>
2025-05-07 16:34:55 +0100

    Add: 'example_embed_large' slash command."

--- main.py
+++ main.py
@@ -101,6 +101,54 @@ async def embed_example(interaction: discord.Interaction):
     await interaction.response.send_message(embed=embed, file=bot_image)
 
 
+# Slash command: /example_embed_large
+@client.tree.command(name="example_embed_large",
+                     description="Creates a maximum sized embedded message.",
+                     guild=GUILD_ID)
+async def embed_example_large(interaction: discord.Interaction):
+    # Grabs file from repository for Discord to use.
+    bot_icon = discord.File("assets/images/bot-image.png",
+                            filename="bot-image.png")
+    camera_icon = discord.File("assets/images/camera.png",
+                               filename="camera.png")
+    embed = discord.Embed(
+        title="📗 **Title:** Maximum 256 characters.",
+        url="https://github.com/MarcusFDev/bot-project",
+        description=" **Description:** Maximum 4096 characters.\n"
+                    "*The total characters a embedded message can have"
+                    " is 6000.*",
+        color=discord.Color.dark_orange())
+    # Uses 'attachment://' to pull image file.
+    embed.set_thumbnail(url="attachment://bot-image.png")
+    embed.add_field(name="\u200b", value="\u200b", inline=False)
+    embed.add_field(
+        name="📖 **Field Title:** Maximum 256 characters.",
+        value="✏️ **Field Content:** 1024 characters.\n\n"
+              "*Total count of Fields allowed is 25!*",
+        inline=False)
+
+    embed.add_field(name="\u200b", value="\u200b", inline=False)
+    embed.add_field(name="\u200b", value="\u200b", inline=False)
+
+    embed.add_field(
+        name="**❓ Quick Tip:**",
+        value="- Using `\\u200b` makes invisible text allowing Field"
+              " name & value to appear empty.\n"
+              "- One Image (not including thumbnail) can be added to a"
+              " embedded message using `.set_image(url=)`.",
+        inline=False)
+    embed.add_field(name="\u200b", value="\u200b", inline=False)
+
+    embed.set_image(url="attachment://camera.png")
+
+    embed.set_footer(text="Footer: Maximum 2048 characters.")
+    embed.set_author(name="Author: Maximum 256 characters.",
+                     url="https://github.com/MarcusFDev/bot-project",
+                     icon_url="attachment://bot-image.png")
+    await interaction.response.send_message(
+        embed=embed, files=[bot_icon, camera_icon])
+
+
 class ButtonArray(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -169,9 +217,9 @@ class EmbedButton(discord.ui.View):
                                            ephemeral=True)
 
 
-# Slash command: /example_button_embed
-@client.tree.command(name="example_button_embed",
-                     description="Creates a button on embedded message.",
+# Slash command: /example_embed_button
+@client.tree.command(name="example_embed_button",
+                     description="Creates a button on a embedded message.",
                      guild=GUILD_ID)
 async def embedbtn_example(interaction: discord.Interaction):
     # Grabs file from repository for Discord to use.
